988de65018c2e7236717e34bf93b3fde3ca00ed9
yeddyfit <jacob_colyvan@live.com>
2021-01-10 23:11:56 +1100

    added search album request logic

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,10 +1,48 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, {useEffect, useState, useContext} from 'react';
+import axios from 'axios';
+// import styled from 'styled-components';
+import UserContext from '../context/UserContext';
+import Button from '@material-ui/core/Button';
 
 import SearchBar from '../components/SearchBar';
 import SearchResults from '../components/SearchResults';
 
+// Searches for albums
+// https://api.spotify.com/v1/search?q=tania%20bowra&type=artist
+
+
+// TODO: add second artist name to tracks tab
+//       seperate followed and owned playlists
+
 const Search = () => {
+  const {token} = useContext(UserContext);
+  const [albums, setAlbums] = useState(false);
+  const [albumName, setAlbumName] = useState(false);
+  const [albumLink, setAlbumLink] = useState(false);
+  const [artist, setArtist] = useState(false);
+
+  // useCallback() here?
+  const getTracks = async () => {
+    try {
+      const tracksResponse = await axios({
+        method: 'get',
+        url: `https://api.spotify.com/v1/search?q=${albumName ? 'album%3A$' + encodeURI(albumName) + '%20' : ''}${artist ? 'artist%3A$' + encodeURI(artist) + '%20' : encodeURI('jon hopkins')}&type=album`,
+        headers: {
+          Authorization: 'Bearer ' + token,
+          'Content-Type': 'application/json'
+        }
+      });
+
+      console.log(tracksResponse.data.albums.items);
+      // setAlbums(tracksResponse.data.albums.items);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  getTracks();
+
+
   return (
     <div>
       <p>SEARCH PAGE</p>
